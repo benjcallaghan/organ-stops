94724a8b4bfe2d7ef3c8538d71b68f2d25454061
Michael Callaghan <mikecallaghan67@gmail.com>
2022-12-27 13:15:13 -0500

    Added nxext and capacitor

---
+++ apps/app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.walkingriver.organstops',
+  appName: 'Organ Stops',
+  webDir: '../../dist/apps/app',
+  bundledWebRuntime: false,
+};
+
+export default config;
