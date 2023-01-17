ab877babbb20a52c585f7c73f6b82087ea00e045
Ben Callaghan <benj.callaghan@gmail.com>
2023-01-16 19:35:51 -0700

    refactor(app): add null checks and assertions to the edit page

--- apps/app/src/app/edit-arrangement/edit-arrangement.page.ts
+++ apps/app/src/app/edit-arrangement/edit-arrangement.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, zip } from 'rxjs';
-import { map, switchMap, take } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { Arrangement, emptyArrangement, emptyHymn, Hymn } from '../hymn';
 
 type EditHymn = {
@@ -27,57 +27,67 @@ export class EditArrangementPage {
     private auth: AngularFireAuth
   ) {
     this.edit$ = route.paramMap.pipe(
-      switchMap((params) =>
-        db
-          .object<Hymn>(`/hymns/${params.get('hymnKey')}`)
+      switchMap((params) => {
+        const hymnKey = params.get('hymnKey');
+        if (!hymnKey) {
+          return of({
+            key: '',
+            hymn: { ...emptyHymn },
+            arrangement: { ...emptyArrangement },
+            readonly: false,
+          });
+        }
+
+        // Either both keys are set, or neither key is set.
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const arrangementKey = params.get('arrangementKey')!;
+        return db
+          .object<Hymn>(`/hymns/${hymnKey}`)
           .valueChanges()
           .pipe(
-            map(
-              (hymn) =>
-                ({
-                  key: params.get('hymnKey'),
-                  hymn: hymn ?? { ...emptyHymn },
-                  arrangement: hymn?.arrangements[
-                    params.get('arrangementKey')
-                  ] ?? { ...emptyArrangement },
-                  readonly: !!hymn,
-                } as EditHymn)
-            )
-          )
-      )
+            map((hymn) => ({
+              key: hymnKey,
+              hymn: hymn ?? { ...emptyHymn },
+              arrangement: hymn?.arrangements[arrangementKey] ?? {
+                ...emptyArrangement,
+              },
+              readonly: !!hymn,
+            }))
+          );
+      })
     );
   }
 
-  save(edit: EditHymn) {
-    zip(this.auth.user, this.route.paramMap)
-      .pipe(take(1))
-      .subscribe(async ([user, params]) => {
-        const hymnKey = params.get('hymnKey');
+  async save(edit: EditHymn) {
+    // The AuthGuard ensures we are always logged in at this page.
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const user = (await this.auth.currentUser)!;
+    const params = this.route.snapshot.paramMap;
 
-        if (edit.arrangement.user.id === user.uid) {
-          // If the user matches, then it must be an existing arrangement.
-          await this.db
-            .object(
-              `/hymns/${hymnKey}/arrangements/${params.get('arrangementKey')}`
-            )
-            .set(edit.arrangement);
-        } else {
-          // If the user does not match, then it must be a new arrangement (possibly existing hymn).
-          const names = user.displayName.split(' ');
-          edit.arrangement.user = {
-            id: user.uid,
-            name: `${names[0]} ${names[names.length - 1][0]}.`,
-          };
+    const hymnKey = params.get('hymnKey');
+    const arrangementKey = params.get('arrangementKey');
 
-          if (hymnKey) {
-            await this.db
-              .list(`/hymns/${hymnKey}/arrangements`)
-              .push(edit.arrangement);
-          } else {
-            const saved = await this.db.list('/hymns').push(edit.hymn);
-            await saved.child('/arrangements').push(edit.arrangement);
-          }
-        }
-      });
+    if (edit.arrangement.user.id === user.uid) {
+      // If the user matches, then it must be an existing arrangement.
+      await this.db
+        .object(`/hymns/${hymnKey}/arrangements/${arrangementKey}`)
+        .set(edit.arrangement);
+    } else {
+      // If the user does not match, then it must be a new arrangement (possibly existing hymn).
+      const names = user.displayName?.split(' ') ?? 'Anonymous';
+      edit.arrangement.user = {
+        id: user.uid,
+        name: `${names[0]} ${names[names.length - 1][0]}.`,
+      };
+
+      if (hymnKey) {
+        await this.db
+          .list(`/hymns/${hymnKey}/arrangements`)
+          .push(edit.arrangement);
+      } else {
+        const saved = await this.db.list('/hymns').push(edit.hymn);
+        await saved.child('/arrangements').push(edit.arrangement);
+      }
+    }
   }
 }
