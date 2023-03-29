583cb37745386a94778e62e3b778f154470f4f90
Ben Callaghan <benj.callaghan@gmail.com>
2023-03-29 11:41:28 -0600

    build: update Angular to v15

--- apps/app/src/app/app-routing.module.ts
+++ apps/app/src/app/app-routing.module.ts
@@ -52,9 +52,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-      relativeLinkResolution: 'legacy',
-    }),
+    preloadingStrategy: PreloadAllModules
+}),
   ],
   exports: [RouterModule],
 })
