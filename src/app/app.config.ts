a3f739321b0badcfd11953a6d2e46692255db09a
Benjamin Callaghan <benj.callaghan@gmail.com>
2026-02-20 21:10:27 -0700

    feat: added Ionic Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,12 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideIonicAngular(),
+  ],
 };
