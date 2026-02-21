beedaa50af5e3e3555430ac93be5412f823350d5
Benjamin Callaghan <benj.callaghan@gmail.com>
2026-02-20 22:04:40 -0700

    feat: added thumbnail and label for collections

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
-import { IonCol, IonContent, IonGrid, IonHeader, IonRow, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  IonThumbnail,
+  IonItem,
+  IonLabel,
+  IonRouterLink,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol],
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonThumbnail,
+    IonItem,
+    IonLabel,
+    RouterLink,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class HomePage {
-  protected items = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
+  protected items = [
+    {
+      id: 1,
+      name: 'First',
+    },
+    {
+      id: 2,
+      name: 'Second',
+    },
+    {
+      id: 3,
+      name: 'Third',
+    },
+    {
+      id: 4,
+      name: 'Fourth',
+    },
+    {
+      id: 5,
+      name: 'Fifth',
+    },
+  ];
 }
